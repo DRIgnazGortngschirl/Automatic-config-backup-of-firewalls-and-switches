fe85af965bfc411521c5330139bd63dead41a927
Mario Michel <1108mario@gmail.com>
2019-03-19 11:18:15 +0100

    Create Backup-Script-Fortinet.sh

---
+++ Backup-Script-Fortinet.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# INSTALLATION
+
+# 1. Start the setup.sh and generate a SSH Key Pair
+# 2. Create a user "backup" on the network device
+# 3. Add SSH Key for Authentification for the user
+
+echo "Started Backup of Config's"
+
+user=backup
+
+for device in `cat ./devices/fortinet-devices.txt | egrep -v "^\s*(#|$)"` # Will have a look in the file "fortinet-devices.txt" for all fortinet devices
+do
+    echo -e "Host found in hostfile \e[35m$device\e[39m"
+    scp $user@$device:sys_config ~/BackupConfigFortinet # Will ssh to all devices in "fortinet-devices.txt" and copy it secure localy 
+    name=`grep -m1 'set hostname' BackupConfigFortinet | sed 's|["?]||g' | sed 's/\<set hostname\>//g' | sed 's/ //g' | tr -dc '[:print:]'`
+    mkdir Archiv/$name >> /dev/null
+    mv BackupConfigFortinet ./Archiv/$name/$name-$(date +"%H:%M-%d.%m.%Y").conf
+done

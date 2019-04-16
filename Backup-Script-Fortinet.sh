bc853d5ad789cb598bb72ba76ca240681ea7f533
Mario Michel <1108mario@gmail.com>
2019-04-16 08:30:24 +0200

    Update Backup-Script-Fortinet.sh

--- Backup-Script-Fortinet.sh
+++ Backup-Script-Fortinet.sh
@@ -8,12 +8,13 @@
 echo "Started Backup of Config's"
 
 user=backup
+date=`date +%d%m%y`
 
 for device in `cat ./Devices/Fortinet-Devices.txt | egrep -v "^\s*(#|$)"` # Will have a look in the file "Fortinet-Devices.txt" for all fortinet devices
 do
-    echo -e "Host found in hostfile \e[35m$device\e[39m"
-    scp -i ./SSH-Keys/Backup-SSH-Key $device:sys_config ./BackupConfigFortinet # Will copy to all devices in "Fortinet-Devices.txt" and copy it secure localy
+    echo -e "Host found in hostfile \e[35m$device\e[39m" &>> Log/log$date.txt
+    scp -i ./SSH-Keys/Backup-SSH-Key $device:sys_config ./BackupConfigFortinet &>> Log/log$date.txt # Will copy to all devices in "Fortinet-Devices.txt" and copy it secure localy
     name=`grep -m1 'set hostname' BackupConfigFortinet | sed 's|["?]||g' | sed 's/\<set hostname\>//g' | sed 's/ //g' | tr -dc '[:print:]'`
-    mkdir Archiv/$name >> /dev/null
+    mkdir Archiv/$name 2>/dev/null
     mv BackupConfigFortinet ./Archiv/$name/$name-$(date +"%H_%M-%d_%m_%Y").conf
 done

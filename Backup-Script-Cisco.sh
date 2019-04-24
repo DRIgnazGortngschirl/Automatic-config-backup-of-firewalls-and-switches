8e603727fc9673873cd9ac97853b23a0d1ee47f5
Mario Michel <1108mario@gmail.com>
2019-04-24 14:45:01 +0200

    Update Backup-Script-Cisco.sh

--- Backup-Script-Cisco.sh
+++ Backup-Script-Cisco.sh
@@ -17,7 +17,7 @@
 user=backup
 date=`date +%d%m%y`
 
-echo "Started Backup of Config's"
+echo "Started Backup of Config's"  &>> Log/log$date.txt
 
 for device in `cat ./Devices/Cicso-Devices.txt | egrep -v "^\s*(#|$)"` # Will have a look in the file "cisco-devices.txt" for all Cisco devices
 do

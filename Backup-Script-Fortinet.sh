0a1dc37fc3a10a920ac742de3e8ffb5bd3739480
Mario Michel <1108mario@gmail.com>
2019-04-25 16:07:20 +0200

    Update Backup-Script-Fortinet.sh

--- Backup-Script-Fortinet.sh
+++ Backup-Script-Fortinet.sh
@@ -6,15 +6,16 @@
 # 2. Add SSH Key for Authentification for the user(No Password!!!)
 
 user=backup
-date=`date +%d%m%y`
+
 
 echo "Started Backup of Config's"
 
 for device in `cat ./Devices/Fortinet-Devices.txt | egrep -v "^\s*(#|$)"` # Will have a look in the file "Fortinet-Devices.txt" for all fortinet devices
 do
+    date=`date +%d%m%y`
     echo -e "Host found in hostfile \e[35m$device\e[39m" &>> Log/Fortinet/log$date.txt
     scp -i ./SSH-Keys/Backup-SSH-Key $device:sys_config ./BackupConfigFortinet &>> Log/Fortinet/log$date.txt # Will copy to all devices in "Fortinet-Devices.txt" and copy it secure localy
     name=`grep -m1 'set hostname' BackupConfigFortinet | sed 's|["?]||g' | sed 's/\<set hostname\>//g' | sed 's/ //g' | tr -dc '[:print:]'` # Will search for the host name to set create a directory and a file named like the hostname of the network device 
-    mkdir Archiv/$name 2>/dev/null
-    mv BackupConfigFortinet ./Archiv/$name/$name-$(date +"%H_%M-%d_%m_%Y").conf # Will move the copied file to the correct folder and will also rename it with the hostname
+    mkdir -v Archiv/$name &>> Log/Fortinet/log$date.txt
+    mv -v BackupConfigFortinet ./Archiv/$name/$name-$(date +"%H_%M-%d_%m_%Y").conf &>> Log/Fortinet/log$date.txt # Will move the copied file to the correct folder and will also rename it with the hostname
 done

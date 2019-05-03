4c1c191abcd48667e764074fadce57590bc732dc
Mario Michel <1108mario@gmail.com>
2019-05-03 14:20:44 +0200

    Update Backup-Script-Fortinet.sh

--- Backup-Script-Fortinet.sh
+++ Backup-Script-Fortinet.sh
@@ -14,7 +14,7 @@ for device in `cat ./Devices/Fortinet-Devices.txt | egrep -v "^\s*(#|$)"` # Will
 do
     date=`date +%d%m%y`
     echo -e "Host found in hostfile \e[35m$device\e[39m" &>> Log/Fortinet/log$date.txt
-    scp -i ./SSH-Keys/Backup-SSH-Key $device:sys_config ./BackupConfigFortinet &>> Log/Fortinet/log$date.txt # Will copy to all devices in "Fortinet-Devices.txt" and copy it secure localy
+    scp -v -i ./SSH-Keys/Backup-SSH-Key $device:sys_config ./BackupConfigFortinet &>> Log/Fortinet/log$date.txt # Will copy to all devices in "Fortinet-Devices.txt" and copy it secure localy
     name=`pv BackupConfigFortinet | grep -m1 'set hostname' | sed 's|["?]||g' | sed 's/\<set hostname\>//g' | sed 's/ //g' | tr -dc '[:print:]'` &>> Log/Fortinet/log$date.txt # Will search for the host name to set create a directory and a file named like the hostname of the network device 
     mkdir -v Archiv/$name &>> Log/Fortinet/log$date.txt
     mv -v BackupConfigFortinet ./Archiv/$name/$name-$(date +"%H_%M-%d_%m_%Y").conf &>> Log/Fortinet/log$date.txt # Will move the copied file to the correct folder and will also rename it with the hostname

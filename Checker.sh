854ef497349f6876753dbc2e364ab21f92f5ff47
Mario Michel <1108mario@gmail.com>
2019-05-13 12:01:50 +0200

    Update Checker.sh

--- Checker.sh
+++ Checker.sh
@@ -5,6 +5,7 @@ hphostfilecount=`egrep -v "^\s*(#|$)" ./Devices/HP-Devices.txt | grep -oE "\b([0
 ciscohostfilecount=`egrep -v "^\s*(#|$)" ./Devices/Cisco-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l`
 dellhostfilecount=`egrep -v "^\s*(#|$)" ./Devices/DELL-Devices.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l`
 dirsinachriv=`find ./Archiv -maxdepth 1 -type d | wc -l`
+configsinachive=`find ./Archiv -maxdepth 2 -type f -mtime -1 | wc -l`
 
 total=`expr $fortinethostfilecount + $hphostfilecount + $ciscohostfilecount + $dellhostfilecount`
 
@@ -13,7 +14,9 @@ echo "HP : $hphostfilecount Hosts in Host File" &>> Log/log$date.txt
 echo "Cisco $ciscohostfilecount Hosts in Host File" &>> Log/log$date.txt
 echo "DELL : $dellhostfilecount Hosts in Host File" &>> Log/log$date.txt
 echo "------------------------------------------------" &>> Log/log$date.txt
-echo "Lines in Host file $total/$dirsinachriv Directorys in Archiv" &>> Log/log$date.txt
+echo "Lines in Host file $total" &>> Log/log$date.txt
+echo "Directorys in Archiv $dirsinachriv" &>> Log/log$date.txt
+echo "Current Configs in Achive $configsinachive" &>> Log/log$date.txt
 echo "These numbers should match!!!" &>> Log/log$date.txt
 echo "Otherwise you did not got all configs listed in the host files" &>> Log/log$date.txt
 echo "Start Fastdebug.sh to see more infos where those errors are" &>> Log/log$date.txt

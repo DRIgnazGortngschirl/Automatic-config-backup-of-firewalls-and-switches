b06a194097b69f9d818649a8065e1923b072be26
Mario Michel <1108mario@gmail.com>
2019-05-14 13:32:14 +0200

    Update Checker.sh

--- Checker.sh
+++ Checker.sh
@@ -8,15 +8,16 @@ dirsinachriv=`find ./Archiv -maxdepth 1 -type d | wc -l`
 configsinachive=`find ./Archiv -maxdepth 2 -type f -mtime -1 | wc -l`
 
 total=`expr $fortinethostfilecount + $hphostfilecount + $ciscohostfilecount + $dellhostfilecount`
-
-echo "Fortinet : $fortinethostfilecount Hosts in Host File" &>> ./Log/BackupCheck/log$date.txt
-echo "HP : $hphostfilecount Hosts in Host File" &>> ./Log/BackupCheck/log$date.txt
-echo "Cisco $ciscohostfilecount Hosts in Host File" &>> ./Log/BackupCheck/log$date.txt
-echo "DELL : $dellhostfilecount Hosts in Host File" &>> ./Log/BackupCheck/log$date.txt
 echo "------------------------------------------------" &>> ./Log/BackupCheck/log$date.txt
-echo "Lines in Host file $total" &>> ./Log/BackupCheck/log$date.txt
-echo "Directorys in Archiv $dirsinachriv" &>> ./Log/BackupCheck/log$date.txt
-echo "Current Configs in Achive $configsinachive" &>> ./Log/BackupCheck/log$date.txt
+echo "Fortinet .. : $fortinethostfilecount Hosts in Host File" &>> ./Log/BackupCheck/log$date.txt
+echo "HP ........ : $hphostfilecount Hosts in Host File" &>> ./Log/BackupCheck/log$date.txt
+echo "Cisco ..... : $ciscohostfilecount Hosts in Host File" &>> ./Log/BackupCheck/log$date.txt
+echo "DELL ...... : $dellhostfilecount Hosts in Host File" &>> ./Log/BackupCheck/log$date.txt
+echo "------------------------------------------------" &>> ./Log/BackupCheck/log$date.txt
+echo "Lines in Host file .......... : $total" &>> ./Log/BackupCheck/log$date.txt
+echo "Directorys in Archiv ........ : $dirsinachriv" &>> ./Log/BackupCheck/log$date.txt
+echo "Current Configs in Achive ... : $configsinachive" &>> ./Log/BackupCheck/log$date.txt
+echo "------------------------------------------------" &>> ./Log/BackupCheck/log$date.txt
 echo "These numbers should match!!!" &>> ./Log/BackupCheck/log$date.txt
-echo "Otherwise you did not got all configs listed in the host files" &>> ./Log/BackupCheck/log$date.txt
+echo "Otherwise you didn't got all configs listed in the host files" &>> ./Log/BackupCheck/log$date.txt
 echo "Start Fastdebug.sh to see more infos where those errors are" &>> ./Log/BackupCheck/log$date.txt
